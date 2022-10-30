486ff042781efa8aba0457833cba0905edd4e603
Afif Akromi <massachusetts48@gmail.com>
2022-10-30 17:58:13 +0700

    Song Details component

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,32 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
+import SongBar from "./SongBar";
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+  artistId,
+}) => (
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+    <div className="mt-6 w-full flex flex-col">
+      {data?.map((song, i) => {
+        return (
+          <SongBar
+            key={`${song?.key}-${artistId}`}
+            song={song}
+            i={i}
+            artistId={artistId}
+            activeSong={activeSong}
+            isPlaying={isPlaying}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        );
+      })}
+    </div>
+  </div>
 );
 
 export default RelatedSongs;
